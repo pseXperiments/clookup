4d80a8b10c5a2900380aeeb1fa6c9286608cf344
DoHoonKim <dohoon1097819@gmail.com>
2024-05-17 02:04:35 +0900

    Update test

--- src/core/test.rs
+++ src/core/test.rs
@@ -3,6 +3,7 @@ mod test {
     use crate::pcs::multilinear::kzg::MultilinearKzg;
     use crate::pcs::PolynomialCommitmentScheme;
     use crate::poly::multilinear::MultilinearPolynomial;
+    use crate::utils::{end_timer, start_timer};
     use crate::utils::{
         random_fe,
         transcript::{InMemoryTranscript, Keccak256Transcript},
@@ -18,8 +19,8 @@ mod test {
 
     #[test]
     pub fn test_clookup() -> Result<(), ProtocolError> {
-        let table_dim = 4;
-        let witness_dim = 2;
+        let table_dim = 16;
+        let witness_dim = 8;
         let table_vec: Vec<Fr> = (0..1 << table_dim).map(|_| random_fe()).collect_vec();
         let witness_vec = table_vec
             .iter()
@@ -33,14 +34,16 @@ mod test {
             let param = ClookupProver::setup(&table, &witness_vec, rng)?;
             MultilinearKzg::trim(&param, 1 << witness_dim, 1).unwrap()
         };
+        let timer = start_timer(|| "clookup prover");
         let proof = {
             let mut transcript = Keccak256Transcript::<Cursor<Vec<u8>>>::default();
             ClookupProver::prove(&pp, &mut transcript, &table, &witness_vec)?;
             transcript.into_proof()
         };
+        end_timer(timer);
         let mut transcript =
             Keccak256Transcript::<Cursor<Vec<u8>>>::from_proof((), proof.as_slice());
-        ClookupVerifier::verify(&vp, &mut transcript, 6, table_dim, witness_dim, max_degree)?;
+        ClookupVerifier::verify(&vp, &mut transcript, table_dim + 2, table_dim, witness_dim, max_degree)?;
         Ok(())
     }
 }
