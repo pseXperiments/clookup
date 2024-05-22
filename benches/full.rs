69ce8e0a8fed2f14b47f99410e7b4527710f806a
Soowon Jeong <soowon1106@gmail.com>
2024-05-22 23:20:18 +0900

    Apply parallelized evaluation

--- benches/full.rs
+++ benches/full.rs
@@ -1,6 +1,14 @@
 use std::{cmp::max, io::Cursor};
 
-use clookup::{core::{precomputation::Table, prover::Prover, verifier::Verifier}, pcs::{multilinear::kzg::MultilinearKzg, PolynomialCommitmentScheme}, utils::{end_timer, random_fe, start_timer, transcript::{InMemoryTranscript, Keccak256Transcript}, ProtocolError}};
+use clookup::{
+    core::{precomputation::Table, prover::Prover, verifier::Verifier},
+    pcs::{multilinear::kzg::MultilinearKzg, PolynomialCommitmentScheme},
+    utils::{
+        end_timer, random_fe, start_timer,
+        transcript::{InMemoryTranscript, Keccak256Transcript},
+        ProtocolError,
+    },
+};
 use criterion::{criterion_group, criterion_main, Criterion};
 use halo2curves::bn256::{Bn256, Fr};
 use itertools::Itertools;
@@ -9,8 +17,8 @@ type ClookupProver = Prover<Fr, MultilinearKzg<Bn256>>;
 type ClookupVerifier = Verifier<Fr, MultilinearKzg<Bn256>>;
 
 pub fn test_clookup() -> Result<(), ProtocolError> {
-    let table_dim = 8;
-    let witness_dim = 4;
+    let table_dim = 16;
+    let witness_dim = 8;
     let table_vec: Vec<Fr> = (0..1 << table_dim).map(|_| random_fe()).collect_vec();
     let witness_vec = table_vec
         .iter()
@@ -31,8 +39,7 @@ pub fn test_clookup() -> Result<(), ProtocolError> {
         transcript.into_proof()
     };
     end_timer(timer);
-    let mut transcript =
-        Keccak256Transcript::<Cursor<Vec<u8>>>::from_proof((), proof.as_slice());
+    let mut transcript = Keccak256Transcript::<Cursor<Vec<u8>>>::from_proof((), proof.as_slice());
     ClookupVerifier::verify(
         &vp,
         &mut transcript,
